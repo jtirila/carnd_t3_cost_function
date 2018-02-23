c14ddf71dd32fdc34b1fd1b42817ea5c9673ed42
Juha-Matti Tirilä <jtirila@gmail.com>
2018-02-23 14:27:01 +0200

    Add Udacity starter code; fix cmakelists

---
+++ cost.hpp
@@ -0,0 +1,6 @@
+#ifndef CARND_COST_COST_HPP
+#define CARND_COST_COST_HPP
+
+float goal_distance_cost(int goal_lane, int intended_lane, int final_lane, float distance_to_goal);
+
+#endif //CARND_COST_COST_HPP
